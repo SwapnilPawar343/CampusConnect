be06e2a9e55a38e065aa400555c66fbce79be8ca
Mrunmai Khode <mrunmaikhode03@gmail.com>
2026-02-04 21:17:23 +0530

    my-ques-page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,17 +1,60 @@
 import React from 'react'
+<<<<<<< Updated upstream
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import Login from './components/Login'
 import Registration from './components/Registration'
+=======
+import StudentDashboard from './Student/pages/StudentDashboard'
+import AlumniDashboard from './Alumni/pages/AlumniDashboard'
+import { BrowserRouter as Router, Routes, Route, Navigate, useLocation } from 'react-router-dom'
+import Login from './Login'
+import Registration from './Registration'
+import QnA from './Student/pages/QnA'
+import MyQues from './Student/pages/MyQues'
+import MentorRecommendation from './Student/pages/MentorRecommendation'
+import CareerPrediction from './Student/pages/CareerPrediction'
+import Navbar from './Student/components/Navbar'
+
+const Layout = ({ children }) => {
+  const location = useLocation()
+  const hideNavbarRoutes = ['/', '/login', '/register']
+  const shouldShowNavbar = !hideNavbarRoutes.includes(location.pathname)
+
+  return (
+    <>
+      {shouldShowNavbar && <Navbar />}
+      {children}
+    </>
+  )
+}
+>>>>>>> Stashed changes
 
 const App = () => {
   return (
     <Router>
+<<<<<<< Updated upstream
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Registration />} />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
+=======
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Registration />} />
+          <Route path="/student-dashboard" element={<StudentDashboard />} />
+          <Route path="/alumni-dashboard" element={<AlumniDashboard />} />
+          <Route path="/qna" element={<QnA />} />
+          <Route path="/my-questions" element={<MyQues />} />
+          <Route path="/mentor-recommendation" element={<MentorRecommendation />} />
+          <Route path="/career-prediction" element={<CareerPrediction />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </Layout>
+>>>>>>> Stashed changes
     </Router>
   )
 }
